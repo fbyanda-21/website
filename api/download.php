5c691f51d983627aef3ce9838de2d4be91b2f304
fbyanda-21 <fubiannanuda@gmail.com>
2025-12-26 00:48:40 +0700

    Update and rename download.php to api/download.php

--- api/download.php
+++ api/download.php
@@ -3,7 +3,7 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
-
+ 
 // Handle preflight requests
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
